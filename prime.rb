9ced4c121a9335101ab927477cbcbc36741a4ac4
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-20 13:34:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(val)
+  prm = false
+  div = true
+  if (val > 3)
+    valSqrt = Math.sqrt(val).to_i
+    div = (2..(valSqrt)).any?{|e| val%e == 0 }
+  elsif (val > 1)
+    div = false
+  end
+  prm = !div
+end
