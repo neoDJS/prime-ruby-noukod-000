6e48cdf3819389021dc1ef7e99103bf1e3c32658
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-20 13:25:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,8 @@ def prime?(val)
     valSqrt = Math.sqrt(val).to_i
     div = (2..(valSqrt)).any?{|e| val%e == 0
   puts "#{val} - #{e} : #{valSqrt} - #{val%e == 0}"
-                                    break if val%e == 0}
+                                    #break if val%e == 0
+                                  }
                                       puts "there1 - #{div}"
   elsif (val > 1)
     puts "there2"
