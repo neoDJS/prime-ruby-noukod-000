38d10149d3798c53977a5cd4d1698cc93aa371aa
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-20 13:33:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,9 +5,7 @@ def prime?(val)
   if (val > 3)
     valSqrt = Math.sqrt(val).to_i
     div = (2..(valSqrt)).any?{|e| val%e == 0 }
-                                      puts "there1 - #{div}"
   elsif (val > 1)
-    puts "there2"
     div = false
   end
   prm = !div
